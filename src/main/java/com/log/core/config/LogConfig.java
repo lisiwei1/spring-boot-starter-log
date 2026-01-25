af8db855bf3dd42a4f18863d0340806ebacba782
lsw <774363190@qq.com>
2026-01-25 22:12:20 +0800

    支持springboot3+版本

--- src/main/java/com/log/core/config/LogConfig.java
+++ src/main/java/com/log/core/config/LogConfig.java
@@ -1,10 +1,10 @@
 package com.log.core.config;
 
+import jakarta.annotation.PostConstruct;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 
 /**
  * @Author lsw
